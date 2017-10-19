2bc5d7595fc590487940742e18dceca02e327bbe
radeknimal <radek.nimal@gmail.com>
2017-10-19 17:48:54 +0200

    dwa kolejne programy

---
+++ program2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+
+	putchar('v'); /* zamiast puts dla pojedynczego znaku stosuje sie putchar i bieze go w pojedynczy cudzyslow */
+	putchar('\n'); /* znak konca linii */
+	puts("wypisz string / ciag znakow");
+	puts("\nAla nie ma kota, \nto kot ma Ale");
+	return 0;
+
+}
+
